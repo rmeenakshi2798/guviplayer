04b46ca28a5003017437e95b7f2b945fffe9fba2
Meenakshi <meenakshirswami39@gmail.com>
2019-02-18 20:26:03 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   char a[100];
+   int i;
+   scanf("%s",a);
+   if(a[0]=='s'&&a[0]=='S')
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+   
+
+    return 0;
+}
