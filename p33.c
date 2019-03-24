cba5b0aca1f901d92763825269c06861f13d2923
Meenakshi <meenakshirswami39@gmail.com>
2019-03-24 11:38:55 +0530

    Create p33.c

---
+++ p33.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[100][100],i,j,c=0,n;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+   }
+ 
+     
+   for(i=0;i<n;i++)
+
+	{
+
+		for(j=0;j<n;j++)
+
+		{
+
+			if(a[i][j]==1)
+
+			{
+
+				if(a[i-1][j]==0&&a[i][j-1]==0&&a[i][j+1]==0&&a[i+1][j]==0)
+
+				{
+
+					c++;
+
+				}
+
+			}
+
+		}
+
+	}
+	printf("%d",c);
+
+    return 0;
+}
