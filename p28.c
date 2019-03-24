2497f6d5d3383f2b885f1f09666cef9aec196ecb
Meenakshi <meenakshirswami39@gmail.com>
2019-03-24 09:47:18 +0530

    Update p28.c

--- p28.c
+++ p28.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-   char a[100],b[100];
+   char a[100],c[100];
    int i,j=0;
   gets(a);
    for(i=0;a[i]!='\0';i++)
    {
        if(a[i]!=' ')
        {
-          b[j]=a[i];
+          c[j]=a[i];
           j++;
        }
        
    }
    b[j]='\0';
-   printf("%s",b);
+   printf("%s",c);
     return 0;
 }
